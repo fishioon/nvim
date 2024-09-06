a3c2d93b04380f46affcc777877c066c17319b1b
ifish <fishioon@live.com>
2024-09-07 01:00:51 +0800

    add statusline

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -5,7 +5,7 @@ local function trim_prefix(str, prefix)
   return vim.fn.pathshorten(vim.fn.fnamemodify(str, ":~:."))
 end
 
-function _G.CustomTabline()
+function _G.MyTabline()
   local tabline = ''
   local tabs = vim.api.nvim_list_tabpages()
   local cur_tab = vim.api.nvim_get_current_tabpage()
@@ -40,16 +40,17 @@ function _G.CustomTabline()
     tabline     = tabline .. hl .. ' ' .. index .. ':' .. count .. mark .. ' ' .. label .. '|' .. name .. ' '
   end
 
-  -- Return the tabline with custom formatting
   return tabline .. '%#TabLineFill#'
+end
+
+local function icon_with(icon, str)
+  return (str == nil or str == '') and '' or icon .. str
 end
 
 ---@return string
 local function lsp_status()
   local attached_clients = vim.lsp.get_clients({ bufnr = 0 })
-  if #attached_clients == 0 then
-    return ""
-  end
+  if #attached_clients == 0 then return "" end
   local it = vim.iter(attached_clients)
   it:map(function(client)
     local name = client.name:gsub("language.server", "ls")
@@ -59,13 +60,23 @@ local function lsp_status()
   return "[" .. table.concat(names, ", ") .. "]"
 end
 
-function _G.statusline()
-  return table.concat({
-    vim.b.minigit_summary_string or '',
-    vim.b.minidiff_summary_string or '',
-    "%f",
+local function diagnostics()
+  local count = vim.diagnostic.count(0)
+  local signs = { 'E', 'W', 'I', 'H' }
+  local s = ''
+  for level, n in pairs(count) do
+    s = s .. ' ' .. signs[level] .. n
+  end
+  return icon_with('', s)
+end
 
+function _G.MyStatusline()
+  return table.concat({
+    "%f",
     "%h%w%m%r",
+    icon_with(' ', vim.b.minigit_summary_string),
+    icon_with(' ', vim.b.minidiff_summary_string),
+    diagnostics(),
     "%=",
     lsp_status(),
     " %-14(%l,%c%V%)",
@@ -73,5 +84,5 @@ function _G.statusline()
   }, " ")
 end
 
-vim.o.statusline = "%{%v:lua._G.statusline()%}"
-vim.o.tabline = '%!v:lua.CustomTabline()'
+vim.o.statusline = "%{%v:lua.MyStatusline()%}"
+vim.o.tabline = '%!v:lua.MyTabline()'
