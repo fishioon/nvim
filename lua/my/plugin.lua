c77268b523b5a1f844262c0e46fe5a33f0af1cbb
ifish <fishioon@live.com>
2025-11-19 15:56:46 +0800

    update

--- lua/my/plugin.lua
+++ lua/my/plugin.lua
@@ -1,10 +1,6 @@
 vim.pack.add({
   { src = 'https://github.com/nvim-mini/mini.nvim' },
   { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
-  { src = 'https://github.com/zbirenbaum/copilot.lua' },
-  -- { src = 'https://github.com/saghen/blink.cmp' },
-  -- { src = 'https://github.com/rafamadriz/friendly-snippets' },
-  -- { src = 'https://github.com/fishioon/cmd.nvim' },
 })
 
 vim.schedule(function()
@@ -16,49 +12,22 @@ vim.schedule(function()
   require('mini.icons').setup()
 
   -- pick
-  require('mini.pick').setup()
-  require('mini.extra').setup()
+  local pick = require('mini.pick')
+  local extra = require('mini.extra')
+  pick.setup()
+  extra.setup()
 
-  MiniPick.registry.projects = function()
+  pick.registry.projects = function()
     local cwd = vim.fn.expand('~/git')
     local choose = function(item)
-      vim.schedule(function() MiniPick.builtin.files(nil, { source = { cwd = item.path } }) end)
+      vim.schedule(function() pick.builtin.files(nil, { source = { cwd = item.path } }) end)
     end
-    return MiniExtra.pickers.explorer({ cwd = cwd }, { source = { choose = choose } })
+    return extra.pickers.explorer({ cwd = cwd }, { source = { choose = choose } })
   end
-
-  require('copilot').setup({
-    suggestion = {
-      enabled = true,
-      auto_trigger = true,
-      hide_during_completion = true,
-      debounce = 50,
-      keymap = {
-        accept = '<tab>',
-      },
-    },
-    filetypes = {
-      markdown = true,
-      ["."] = false,
-    },
-    copilot_node_command = 'node', -- Node.js version must be > 18.x
-  })
-
-  -- require('blink.cmp').setup({
-  --   keymap = { preset = 'enter' },
-  --   completion = { documentation = { auto_show = true } },
-  --   cmdline = {
-  --     keymap = { preset = 'cmdline' },
-  --     completion = { menu = { auto_show = true } },
-  --   },
-  --   sources = {
-  --     default = { 'lsp', 'path', 'snippets', 'buffer' },
-  --   },
-  --   fuzzy = { implementation = "prefer_rust_with_warning" },
-  -- })
 
   -- lsp config
   vim.diagnostic.config({ virtual_text = true })
+
   vim.lsp.config('*', {
     root_markers = { '.git' },
   })
@@ -66,7 +35,9 @@ vim.schedule(function()
   vim.lsp.config('tsls',
     { cmd = { 'typescript-language-server', '--stdio' }, filetypes = { 'javascript', 'typescript' } })
   vim.lsp.config('yamls', { cmd = { 'yaml-language-server', '--stdio' }, filetypes = { 'yaml' } })
-  vim.lsp.enable({ 'luals', 'gopls', 'tsls', 'jsonls' })
+  vim.lsp.config('copilot',
+    { cmd = { 'copilot-language-server', '--stdio' }, filetypes = { 'markdown', 'go', 'lua', 'json' } })
+  vim.lsp.enable({ 'luals', 'gopls', 'tsls', 'jsonls', 'copilot' })
 
   require('nvim-treesitter').install { 'go', 'markdown', 'markdown_inline', 'javascript', 'typescript', 'json', 'yaml', 'lua' }
   require('nvim-treesitter').setup({
@@ -78,11 +49,27 @@ end)
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('my.lsp', {}),
   callback = function(args)
+    local bufnr = args.buf
     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
     if client:supports_method('textDocument/completion') then
       vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
     end
 
+    if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlineCompletion, bufnr) then
+      vim.lsp.inline_completion.enable(true, { bufnr = bufnr })
+      vim.keymap.set(
+        'i',
+        '<tab>',
+        vim.lsp.inline_completion.get,
+        { desc = 'LSP: accept inline completion', buffer = bufnr }
+      )
+      vim.keymap.set(
+        'i',
+        '<C-l>',
+        vim.lsp.inline_completion.select,
+        { desc = 'LSP: switch inline completion', buffer = bufnr }
+      )
+    end
     -- Auto-format ("lint") on save.
     -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
     -- if not client:supports_method('textDocument/willSaveWaitUntil')
